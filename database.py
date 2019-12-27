0c80942d8b4a7495ef3c28f449321efc39a81d72
pranjal <pranjalwalia77@gmail.com>
2019-12-27 18:23:36 +0530

    added database file

---
+++ database.py
@@ -0,0 +1,6 @@
+data = {
+    "grading students" : "grading",
+    "designer pdf viewer" : "designer-pdf-viewer",
+    'cut the sticks' : 'cut-the-sticks',
+    'determining dna health' : 'determining-dna-health'
+}
