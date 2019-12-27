f8d37b0c13c6d613345899963f22ffe34cacbc4f
pranjal <pranjalwalia77@gmail.com>
2019-12-27 19:47:43 +0530

    unresolved import

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from database import *
+
 
 #for chrome
 driver = webdriver.Chrome()
